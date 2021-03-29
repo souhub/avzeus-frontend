a10cf117d6aae5c10614724c3132622e715dd761
souhub <ace0804sou@gmail.com>
2021-03-29 20:25:03 +0900

    Show selected wemen images

--- src/components/Selection.vue
+++ src/components/Selection.vue
@@ -11,9 +11,11 @@
       </div>
       <a class="btn" href="/recommendation">人選んで次へ</a>
       <button class="btn" onclick="history.back()" style="width:100%; background-color: gray">戻る</button>
-      <form action="" v-for="(woman,index) in this.$root.wemen" :key=index>
-         <div>
-            <input type="checkbox" name="selected_woman" value=":index"><img :src="woman.image_path" alt="女性の画像">
+      <form action="" >
+         <div class="box">
+            <div class="child" v-for="(woman,index) in this.$root.wemen" :key=index>
+               <input type="checkbox" name="selected_woman" :value="index"><img class="child-img" :src="woman.image_path" alt="女性の画像">
+            </div>
          </div>
       </form>
    </div>
@@ -21,3 +23,19 @@
 
 <script scoped>
 </script>
+
+<style scoped>
+.box {
+  display: flex;
+  flex-wrap: wrap;
+  height: 400px;
+  align-content: flex-start;
+}
+.child{
+   width: 30%;
+}
+
+.child-img{
+   width: 100%;
+}
+</style>
