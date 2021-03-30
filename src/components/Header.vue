d326e19d769f456924752f2fafca09190dff390f
souhub <ace0804sou@gmail.com>
2021-03-30 12:15:14 +0900

    Updated header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h2 style="font-family:Montserrat; line-height:0px; font-size:25px">AV Zeus</h2>
+        <a href="/"><h2 style="font-family:Montserrat; line-height:0px; font-size:25px">AV Zeus</h2></a>
         <p style="line-height: 0px; font-size: 7px ">AV人工痴能エンジン</p>
     </div>
 </template>
