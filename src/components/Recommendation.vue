eb5632000f105d70c0b83d8c6d4e9829a968da66
souhub <ace0804sou@gmail.com>
2021-04-02 14:01:38 +0900

    Updated Recommendation and Selection

--- src/components/Recommendation.vue
+++ src/components/Recommendation.vue
@@ -1,21 +1,37 @@
 <template>
-<div>
-   <div id="fukidashi-wrapper">
-      <div id="fukidashi">
-         <p>
-            全知全能のワシが分析したお主に最適のAVはこれじゃ！！
-         </p>
+   <div>
+      <div id="fukidashi-wrapper">
+         <div id="fukidashi">
+            <p>
+               全知全能のワシが分析したお主に最適のAVはこれじゃ！！
+            </p>
+         </div>
+         <img id="zeus" src="img/zeus.png" alt="zeus">
       </div>
-      <img id="zeus" src="img/zeus.png" alt="zeus">
-   </div>
-   <form action="" v-for="(actress,index) in this.$root.actresses" :key=index>
       <div>
-         <a href=""><input type="hidden" name="actress" value=":index"><img :src="actress.image_path" alt="女優の画像"></a>
+      <form action="localhost:8000/result" method-="POST">
+         <div v-for="(actress,index) in this.$root.actresses" :key=index>
+        <button type="submit">SUBMIT</button >
+            <input type="hidden" name="actress" value=":index">
+            <input type="text" name="id" :value="this.$route.query.id">
+            <img :src="actress.image_path" alt="女優の画像">
+         </div>
+      <!-- テスト -->
+      <p>ids(AIに返すTraining用ID): {{this.$route.query.id}}</p>
+      <p>ids(これをもとに女優データをVueからAPIアクセスで取得): {{this.$route.query.ids}}</p>
+      </form>
       </div>
-   </form>
-   <a class="btn" href="/">ありがとうございました！</a>
-</div>
+      <a class="btn" href="/">ありがとうございました！</a>
+   </div>
 </template>
 
 <script>
+export default {
+  props: {
+    id: {
+      type: String,
+      required: false
+    }
+  }
+};
 </script>
--- src/components/Selection.vue
+++ src/components/Selection.vue
@@ -1,65 +1,129 @@
 <template>
-   <div id="selection">
-      <div id="fukidashi-wrapper">
-         <div id="fukidashi">
-            <p>
-               下の9人のおなごの中から好きなおなごを<span class="attention" >かわいいと思う順に最大5人まで</span>選ぶのじゃ。<br/>
-               これを元におぬし好みのAVを授けるぞい！
-            </p>
-         </div>
-         <img id="zeus" src="img/zeus.png" alt="zeus">
+  <div id="selection">
+    <div id="fukidashi-wrapper">
+      <div id="fukidashi">
+        <p>
+          下の9人のおなごの中から好きなおなごを<span class="attention"
+            >かわいいと思う順に5人</span
+          >選ぶのじゃ。<br />
+          これを元におぬし好みのAVを授けるぞい！
+        </p>
+        <p
+          v-if="selectedWemen.length > 5"
+          style="font-weight: bold; color: red"
+        >
+          5人以上選んでいるぞい！かわいい順に5人選び直すのじゃ！
+        </p>
+        <p v-else>
+          <span style="font-weight:bold; font-size:20px">{{
+            selectedWemen.length
+          }}</span
+          >人選択中じゃ。<br />あと<span
+            style="font-weight:bold; font-size:20px"
+            >{{ 5 - selectedWemen.length }}</span
+          >人選択するのじゃ。
+        </p>
       </div>
+      <img id="zeus" src="img/zeus.png" alt="zeus" />
+    </div>
 
-         <form action="http://localhost:8000/select" method="post">
-            <div class="box">
-               <div class="child" v-for="(woman,index) in this.$root.wemen" :key=index>
-                  <input type="checkbox" :id=index name="selected_woman" :value=index v-model="selectedWemen">
-                  <p class="index">{{index}}</p>
-                  <label class="check-box" :for=index>
-                     <img class="child-img" :src="woman.image_path" alt="女性の画像">
-                  </label>
-               </div>
-            </div>
-            <p id="std">
-               <i id="like" class="fas fa-heart"></i> ←ーーーーーーー→ <i id="dislike" class="fas fa-heart-broken"></i><br/>
-               <span>{{selectedWemen}}</span>
-            </p>
-         </form>
+    <!-- checkbox だと配列の順番がランダムで変わるので隠しフォームで送信する -->
 
+    <!-- ダミーフォーム -->
+    <div class="box">
+      <div
+        class="child"
+        v-for="(woman, index) in this.$root.wemen"
+        :key="index"
+      >
+        <input
+          type="checkbox"
+          :id="index"
+          name="selected_wemen"
+          :value="index"
+          v-model="selectedWemen"
+        />
+        <p class="index">{{ index }}</p>
+        <label class="check-box" :for="index">
+          <img class="child-img" :src="woman.image_path" alt="女性の画像" />
+        </label>
+      </div>
+    </div>
+
+    <!-- <p id="std">
+         <i id="like" class="fas fa-heart"><i id="dislike" class="fas fa-heart-broken"></i><br/>
+         <span>{{selectedWemen}}</span>
+      </p> -->
+
+    <!-- 本物のフォーム（隠しフォーム） -->
+    <form action="http://localhost:8000/selection" method="POST">
       <div class="btns">
-         <a class="btn" href="/recommendation">{{selectedWemen.length}}人選んで次へ</a>
-         <button class="btn" onclick="history.back()" style="width:100%; background-color: gray">戻る</button>
+        <button v-if="selectedWemen.length == 5" class="btn" type="submit">
+          <span v-if="selectedWemen.length == 5">結果を見る</span>
+        </button>
+        <a
+          v-if="selectedWemen.length != 5"
+          class="btn"
+          style="background-color:#34b871"
+        >
+          <span v-if="selectedWemen.length < 5"
+            >あと{{ 5 - selectedWemen.length }}人選ぶのじゃ！</span
+          >
+          <span v-else-if="selectedWemen.length > 5"
+            >選び過ぎじゃ！5人選ぶのじゃ</span
+          >
+        </a>
+        <input
+          class="btn"
+          type="button"
+          @click="reset"
+          value="選びなおす"
+          style="background-color:#FFCC00; width:100%"
+        />
+        <button
+          class="btn"
+          onclick="history.back()"
+          style="background-color: gray; width:100%"
+        >
+          戻る
+        </button>
       </div>
-
-   </div>
+      <input type="hidden" name="selected_wemen" :value="selectedWemen" />
+    </form>
+  </div>
 </template>
 
 <script scoped>
-export default{
-   data(){
-      return {
-         selectedWemen: []
-      }
-   }
-}
+export default {
+  data() {
+    return {
+      selectedWemen: [],
+    };
+  },
+  methods: {
+    reset: function() {
+      this.selectedWemen = [];
+    },
+  },
+};
 </script>
 
 <style scoped>
-#like{
-   color:palevioletred;
+#like {
+  color: palevioletred;
 }
 
-#dislike{
-   color: black;
+#dislike {
+  color: black;
 }
 
-#std{
-   text-align: center;
+#std {
+  text-align: center;
 }
 
-.attention{
-   font-weight: 900;
-   color: red;
+.attention {
+  font-weight: 900;
+  color: red;
 }
 
 .box {
@@ -69,29 +133,28 @@ export default{
   text-align: center;
 }
 
-.child{
-   width: 33%;
+.child {
+  width: 33%;
 }
 
-.child-img{
-   width: 100%;
-   max-width: 200px;
+.child-img {
+  width: 100%;
+  max-width: 200px;
 }
 
-.index{
-   line-height: 0;
+.index {
+  line-height: 0;
 }
 
-input[type="checkbox"]{
-   display: none;
+input[type="checkbox"] {
+  display: none;
 }
 /* checkされたときのCSS */
-input:checked+.index{
-color: #5ae099;
+input:checked + .index {
+  color: #5ae099;
 }
 
-input:checked ~ * .child-img{
-   opacity: 0.7;
+input:checked ~ * .child-img {
+  opacity: 0.7;
 }
-
 </style>
