00def269c010706b15d3fa3f4b7ecbb96f7a32d8
souhub <ace0804sou@gmail.com>
2021-04-15 20:36:24 +0900

    Added dmm credit

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,9 @@
 <template>
     <footer>
+        <p>
+            <a href="https://affiliate.dmm.com/api/"><img src="https://p.dmm.co.jp/p/affiliate/web_service/r18_135_17.gif" width="135" height="17" alt="WEB SERVICE BY FANZA" /></a>
+        </p>
+
         <p class="copyright">©2021 AVZeus</p>
     </footer>
 </template>
