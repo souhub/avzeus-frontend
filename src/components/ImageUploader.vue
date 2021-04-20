103e24dd40641c1c86845fa6fa484f79859ee7ad
souhub <ace0804sou@gmail.com>
2021-04-20 10:16:48 +0900

    Updated image-uploader page

--- src/components/ImageUploader.vue
+++ src/components/ImageUploader.vue
@@ -2,7 +2,8 @@
     <div>
          <div id="fukidashi-wrapper">
             <div id="fukidashi">
-                <div v-if="!imagePath">
+                <div v-if="!loading">
+                    <div v-if="!imagePath">
                     <div v-if="!previewURL">
                         <p>お主の好みのおなごの写真をアップロードしてくれい！</p>
                         <p>この写真を元にお主好みのAV女優を教えるぞい！</p>
@@ -11,13 +12,15 @@
                         <p>この写真でいいかの？</p>
                         <p>よければワシがこの写真からおなごの顔だけを切り抜くぞい！</p>
                     </div>
-                    <!-- <div v-if="loading">
-                        <p>切り抜き中じゃ！もう少しそのまま待ってくれい</p>
-                    </div> -->
                 </div>
                 <div v-if="imagePath">
                     <p>切り抜いたぞい！アップロードした写真はこの娘でいいかの？</p>
                     <p>この娘でよければ結果を見るボタンを押してくれい！</p>
+                </div>
+                </div>
+
+                <div v-if="loading">
+                        <p>切り抜き中じゃ！もう少しそのまま待ってくれい</p>
                 </div>
             </div>
             <img id="zeus" src="https://storage.googleapis.com/avzeus/zeus.png" alt="zeus">
@@ -25,20 +28,19 @@
 
         <div v-if="imagePath" style="text-align: center;">
             <img :src="imagePath" style="margin-top:50px" />
-            <input @click="recommendation"
-                class="btn submit-button"
-                type="button"
-                value="結果を見る"
-            />
+            <form action="/recommendation" method="GET">
+                <input name="selected_wemen_ids" type="hidden" :value="this.ids">
+                <input @click="s3DeleteFile" class="btn submit-button" type="submit" value="結果を見る"/>
+            </form>
         </div>
 
         <vue-loading v-if="loading" type="spiningDubbles" color="white" :size="{ width: '150px', height: '150px' }"></vue-loading>
 
-        <div v-if="!imagePath">
+        <div v-if="!imagePath" v-show="!loading">
             <form action="/api/image-clipping" method="POST" enctype="multipart/form-data">
                 <div class="preview-wrapper">
                     <div class="preview-box" v-if="previewURL">
-                        <img class="image-preview" :src="previewURL">
+                        <img class="image-preview" :src="previewURL" v-if="!loading">
                     </div>
                     <div style="height: 250px" v-else>
                         <label id="label" for="image">
@@ -56,7 +58,7 @@
                     />
                     <input
                         v-if="previewURL"
-                        @click="isLoading && isSubmitting"
+                        @click="isLoading"
                         class="btn submit-button"
                         type="submit"
                         value="切り抜いてもらう"
@@ -86,7 +88,9 @@ export default {
             previewURL: '',
             ids: this.$route.query.ids,
             imagePath: this.$route.query.image_path,
+            s3ObjectKey: this.$route.query.s3_object_key,
             loading: false,
+            redirectURL: String
         }
     },
     methods:{
@@ -98,21 +102,16 @@ export default {
             return this.previewURL=''
         },
         isLoading(){
-            return this.loading=true
+            this.loading=true
         },
-        recommendation(){
+        s3DeleteFile(){
+            console.log('s3DeleteFile is working')
             axios
-                .post('/api/recommendation',{
-                    // selected_wemen_ids:'1,2,3,4,5'
-                    selected_wemen_ids:this.ids
-                })
-                .then(response=>{
-                this.trainingID=response.data.training_id,
-                this.recommendedActresses=response.data.recommended_actresses,
-                this.loading=false
+                .delete('/api/s3-delete-file',{data:{"s3_object_key":this.s3ObjectKey}})
+                .then((response)=>{
+                    console.log(response.data.message)
                 })
                 .catch((err)=>{console.log(err)})
-                .finally(()=>this.loading=false)
         }
     },
     components:{
